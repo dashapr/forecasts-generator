0ed9f013279ab3dcb70dcef4640dc6583cf87718
dashapr <dashaprud@rambler.ru>
2022-11-29 18:19:31 +0300

    Домашнее задание 5

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
+const magicButton = document.querySelector('.forecast-btn');
+const newPrediction = document.querySelector('.current-forecast h1');
+const newPercent = document.querySelector('.current-forecast p');
+const forecastsList = document.querySelector('.forecasts');
+const cardTemplate = document.querySelector('#forecast-item');
+
+
+
+
+magicButton.addEventListener("click", createPrediction);
+
+function createPrediction() {
+
+    switch (randomInteger(0, 3)) {
+        case 0:
+            newPrediction.textContent = firstPrediction;
+
+            break;
+        case 1:
+            newPrediction.textContent = secondPrediction;
+
+            break;
+        case 2:
+            newPrediction.textContent = thirdPrediction;
+
+            break;
+        default:
+            newPrediction.textContent = fourthPrediction;
+
+    }
+    newPercent.textContent = `Вероятность: ${randomInteger(0, 100)}%`;
+
+    return forecastsList.prepend(makeCardByTemplate(newPrediction.textContent, newPercent.textContent));
+}
+
+
+
+function makeCardByTemplate(prd, prc) {
+    const myCard = cardTemplate.content.cloneNode(true);
+
+    myCard.querySelector('h3').textContent = prd;
+    myCard.querySelector('p').textContent = prc;
+
+    return myCard;
+}
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
@@ -13,3 +58,14 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
+const firstPrediction = 'Сегодня вечером тебя ожидает приятный сюрприз';
+const secondPrediction = 'Скоро будет прибавка в зарплате';
+const thirdPrediction = 'Все будет хорошо';
+const fourthPrediction = 'Ты найдешь то, что искала';
